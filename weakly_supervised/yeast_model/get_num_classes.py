ac6f332cbcd1b1849f4acc21f247ce3a1eb96b63
LZhang98 <luke.taotao@hotmail.com>
2021-02-06 17:19:56 -0500

    num_classes retrieval

---
+++ weakly_supervised/yeast_model/get_num_classes.py
@@ -0,0 +1,17 @@
+# TEMP SCRIPT TO GET NUM CLASSES
+
+# INCORPORATE INTO TRAIN.PY LATER
+
+import opts as opt
+from dataset import Dataset
+
+ds = Dataset()
+num_classes = ds.add_dataset(opt.data_path)
+
+print("Number of classes:", num_classes)
+
+f = open("num_classes.txt", "w")
+
+f.write(num_classes)
+
+f.close()
