b6762fdc5f31249d0d3aaa0cba97419cba0a2624
LZhang98 <luke.taotao@hotmail.com>
2021-02-08 18:15:09 -0500

    full layers extraction

--- weakly_supervised/yeast_model/extract_features.py
+++ weakly_supervised/yeast_model/extract_features.py
@@ -44,44 +44,44 @@ if __name__ == "__main__":
 
     print(model.summary())
 
-    # for layer in conv_layers:
+    for layer in conv_layers:
         
-    #     intermediate_model = tf.keras.Model(inputs=model.get_layer("x_in").input,
-    #                                         outputs=model.get_layer(layer).output)
+        intermediate_model = tf.keras.Model(inputs=model.get_layer("x_in").input,
+                                            outputs=model.get_layer(layer).output)
 
-    #     print ("Evaluating images...")
-    #     # Load each single cell and extract features into a file
-    #     for dir in os.listdir(datapath):
-    #         for image in os.listdir(datapath + dir):
-    #             if "_gfp.tif" in image:
-    #                 print ("Evaluating", image)
-    #                 # Iterate over every single cell crop and preprocess it
-    #                 name = image.rsplit("_", 1)[0]
-    #                 gfp = np.array(Image.open(datapath + dir + "/" + image)).astype(np.float32)
-    #                 rfp = np.array(Image.open(datapath + dir + "/" + image.replace("gfp", "rfp"))).astype(np.float32)
+        print ("Evaluating images...")
+        # Load each single cell and extract features into a file
+        for dir in os.listdir(datapath):
+            for image in os.listdir(datapath + dir):
+                if "_gfp.tif" in image:
+                    print ("Evaluating", image)
+                    # Iterate over every single cell crop and preprocess it
+                    name = image.rsplit("_", 1)[0]
+                    gfp = np.array(Image.open(datapath + dir + "/" + image)).astype(np.float32)
+                    rfp = np.array(Image.open(datapath + dir + "/" + image.replace("gfp", "rfp"))).astype(np.float32)
 
-    #                 gfp = skimage.exposure.rescale_intensity(gfp, out_range=(0, 1))
-    #                 rfp = skimage.exposure.rescale_intensity(rfp, out_range=(0, 1))
+                    gfp = skimage.exposure.rescale_intensity(gfp, out_range=(0, 1))
+                    rfp = skimage.exposure.rescale_intensity(rfp, out_range=(0, 1))
 
-    #                 # Feed single cell crop into the pretrained model and obtain features
-    #                 x_in = np.stack((gfp, rfp), axis=-1)
-    #                 x_in = np.expand_dims(x_in, axis=0)
+                    # Feed single cell crop into the pretrained model and obtain features
+                    x_in = np.stack((gfp, rfp), axis=-1)
+                    x_in = np.expand_dims(x_in, axis=0)
 
-    #                 prediction = intermediate_model.predict([x_in], batch_size=1)
+                    prediction = intermediate_model.predict([x_in], batch_size=1)
 
-    #                 prediction = np.squeeze(prediction)
-    #                 prediction = np.max(prediction, axis=(0, 1))
+                    prediction = np.squeeze(prediction)
+                    prediction = np.max(prediction, axis=(0, 1))
 
-    #                 # Write features into a file
-    #                 outputfile = opt.checkpoint_path + "yeast_features_" + layer + ".txt"
-    #                 output = open(outputfile, "a")
-    #                 output.write(name)
-    #                 output.write("\t")
-    #                 for feat in prediction:
-    #                     output.write(str(feat))
-    #                     output.write("\t")
-    #                 output.write("\n")
-    #                 output.close()
+                    # Write features into a file
+                    outputfile = opt.checkpoint_path + "yeast_features_" + layer + ".txt"
+                    output = open(outputfile, "a")
+                    output.write(name)
+                    output.write("\t")
+                    for feat in prediction:
+                        output.write(str(feat))
+                        output.write("\t")
+                    output.write("\n")
+                    output.close()
 
     for layer in flat_layers:
         
@@ -106,18 +106,9 @@ if __name__ == "__main__":
                     x_in = np.stack((gfp, rfp), axis=-1)
                     x_in = np.expand_dims(x_in, axis=0)
 
-                    print(x_in.shape)
-
                     prediction = intermediate_model.predict([x_in], batch_size=1)
 
-                    print(prediction.shape)
-
                     prediction = np.squeeze(prediction)
-
-                    print(prediction.shape)
-
-                    # prediction = np.max(prediction, axis=(0, 1))
-                    # print(prediction.shape)
 
                     # Write features into a file
                     outputfile = opt.checkpoint_path + "yeast_features_" + layer + ".txt"
