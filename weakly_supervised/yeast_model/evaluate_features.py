c49b68f12ffc7326dcedc5fec07be0f128513d39
LZhang98 <luke.taotao@hotmail.com>
2021-02-09 14:28:07 -0500

    feature vis

--- weakly_supervised/yeast_model/evaluate_features.py
+++ weakly_supervised/yeast_model/evaluate_features.py
@@ -2,6 +2,17 @@ import umap
 import numpy as np
 import umap.plot
 
+file_list = ['conv1_1.txt',
+            'conv2_1.txt',
+            'conv3_1.txt',
+            'conv4_1.txt',
+            'conv5_1.txt',
+            'dense1.txt',
+            'dense2.txt']
+
 data = np.genfromtxt(fname="test/yeast_features_conv1_1.txt", delimiter="\t")
 
 print(data.shape)
+
+row_names = data[:,0]
+print(row_names[0:5])
