92988237e6ecad18ade3bbaa5d3e6e52d0d1d94f
LZhang98 <luke.taotao@hotmail.com>
2021-02-09 18:09:59 -0500

    feature vis

--- weakly_supervised/yeast_model/evaluate_features.py
+++ weakly_supervised/yeast_model/evaluate_features.py
@@ -1,18 +1,34 @@
 import umap
 import numpy as np
 import pandas
-import umap.plot
+import matplotlib.pyplot as plt
 
-file_list = ['conv1_1.txt',
-            'conv2_1.txt',
-            'conv3_1.txt',
-            'conv4_1.txt',
-            'conv5_1.txt',
-            'dense1.txt',
-            'dense2.txt']
+file_list = ['conv1_1',
+            'conv2_1',
+            'conv3_1',
+            'conv4_1',
+            'conv5_1',
+            'dense1',
+            'dense2']
 
-data = pandas.read_csv('test/yeast_features_'+file_list[0], sep = '\t', )
 
-print(data.shape)
+for i in range(len(file_list)):
+    f = file_list[i]
+    data = pandas.read_csv('test/yeast_features_'+f+'.txt', sep = '\t', )
 
-print(data.axes)
+    data.set_index(data.columns[0])
+
+    data.drop(data.columns[len(data.columns)-1], axis=1, inplace=True)
+
+    print(data)
+
+    fit = umap.UMAP()
+    u = fit.fit_transform(data)
+
+    print(u)
+
+    x = u[:,0]
+    y = u[:,1]
+    plt.figure(i)
+    plt.scatter(x, y, marker='.')
+    plt.savefig('test/yeast_features_'+f+'_plot.png')
